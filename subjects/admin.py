de53a4256b928d77261c3f70e7f0d903a0b3b565
nirvanajoshi <damodarj36@gmail.com>
2026-08-11 19:20:13 +0545

    Registration of Models in admin.py of Subject

--- subjects/admin.py
+++ subjects/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Subject
 
-# Register your models here.
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ("name", "code", "teacher", "semester")
+    list_filter = ("semester",)
+    search_fields = (
+        "name",
+        "code",
+        "teacher__username",
+        "teacher__first_name",
+        "teacher__last_name",
+    )
+    ordering = ("code",)
