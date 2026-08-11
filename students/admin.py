b110568f62cd266748c93dc4d092bdc182ee46ba
nirvanajoshi <damodarj36@gmail.com>
2026-08-11 19:16:59 +0545

    Registration of Models in admin.py of Student and Account

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Student
 
-# Register your models here.
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("user", "student_id", "semester", "created_at")
+    list_filter = ("semester", "gender")
+    search_fields = (
+        "student_id",
+        "user__username",
+        "user__email",
+        "user__first_name",
+        "user__last_name",
+    )
+    ordering = ("student_id",)
