bef277b7f7d375f9326c9c83fe104d3b22047613
nirvanajoshi <damodarj36@gmail.com>
2026-08-11 19:22:56 +0545

    Registration of Models in admin.py of Attendance

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Attendance
 
-# Register your models here.
+
+@admin.register(Attendance)
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = (
+        "student",
+        "subject",
+        "date",
+        "status",
+        "marked_by",
+    )
+
+    list_filter = (
+        "date",
+        "status",
+        "subject",
+    )
+
+    search_fields = (
+        "student__student_id",
+        "student__user__username",
+        "subject__name",
+        "subject__code",
+        "marked_by__username",
+    )
+
+    ordering = ("-date",)
